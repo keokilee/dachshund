e60267a8d2f9f6e0eff10682143fe8964a1ca350
George Lee <keoki.lee@gmail.com>
2015-11-10 21:49:44 -1000

    Update docs and add bin and lib folder.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dachshund
+
+Convert the underscores in a file/directory name to dashes or convert the dashes to underscores.
+
+## Usage
+
+`npm install -g dachshund`
+
+Installing `dachshund` globally adds two commands `udash` and `dunder` to your PATH.
+
+Convert underscores to dashes:
+```
+udash file_name
+#=> file_name => file-name
+```
+
+Convert dashes to underscores:
+```
+dunder file_name
+#=> file-name => file_name
+```
--- index.js
+++ index.js
@@ -1,63 +1,35 @@
 'use strict';
 
-const fs = require('fs');
 const program = require('commander');
+const dachshund = require('./lib/dachshund');
 
-function dashize(file, suppress) {
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (!suppress) {
-        console.error('Path does not exist.');
-      }
+module.exports = {
+  underToDash: underToDash,
+  dashToUnder: dashToUnder
+};
 
-      return;
-    }
+function underToDash(args) {
+  program.parse(args);
 
-    if (stats.isFile() || stats.isDirectory()) {
-      const newPath = file.replace('_', '-');
-      if (file === newPath && !suppress) {
-        console.err(`No need to change ${newPath}`);
-      } else {
-        fs.rename(file, newPath, (err, _) => {
-          console.log(`${file} => ${newPath}`);
-        });
-      }
-    }
-  });
-}
-
-function underscorize(file, suppress) {
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (!suppress) {
-        console.error("Path '${file}' does not exist.");
-      }
-      return;
-    }
-
-    if (stats.isFile() || stats.isDirectory()) {
-      const newPath = file.replace('-', '_');
-      if (file !== newPath) {
-        fs.rename(file, newPath, (err, _) => {
-          console.log(`${file} => ${newPath}`);
-        });
-      }
-    }
-  });
-}
-
-function main() {
-  program.parse(process.argv);
-  let args = program.args;
-
-  if (args.length === 0) {
+  if (program.args.length === 0) {
     console.error('A path is required');
     return;
   }
 
-  args.forEach(dashize);
+  program.args.forEach(dachshund.dashize);
+}
+
+function dashToUnder(args) {
+  program.parse(args);
+
+  if (program.args.length === 0) {
+    console.error('A path is required');
+    return;
+  }
+
+  program.args.forEach(dachshund.underscorize);
 }
 
 if (require.main === module) {
-  main();
+  dashToUnder(process.argv);
 }
